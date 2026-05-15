ede70c9e214b8b9a015c8724e2edfcf91d6d00e1
Florent Segouin <florent@segouin.me>
2026-05-15 23:36:35 +0200

    chore(test): remove unused userEvent import in DegradationBanner test

--- packages/web/test/components/DegradationBanner.test.tsx
+++ packages/web/test/components/DegradationBanner.test.tsx
@@ -1,6 +1,5 @@
 import { describe, expect, it, beforeEach, vi, afterEach } from 'vitest';
 import { render, screen, act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { useConnectionStore } from '../../src/state/connection.js';
 import { DegradationBanner } from '../../src/components/shell/DegradationBanner.js';
 
